f61610ce4433afd63bf83ffac69a99c996ad0f73
Phil Freeman <paf31@cantab.net>
2015-02-28 16:22:45 -0800

    An implementation based on simple arrowized FRP

--- test/Main.purs
+++ test/Main.purs
@@ -7,8 +7,9 @@ import Control.Monad.Eff
 
 import DOM
 
-import qualified Halogen as H
-import qualified Halogen.HTML as H
+import Halogen
+import Halogen.Signal
+import Halogen.HTML 
 
 foreign import appendToBody
   "function appendToBody(node) {\
@@ -19,42 +20,12 @@ foreign import appendToBody
 
 data ButtonEvent = Click
 
--- | This `Spec` defines a reusable button component, which has a single child
--- | placeholder, and supports click events.
--- | 
--- | Its state is a `Number` which represents the number of clicks
-button :: forall c. H.Spec Number c -> H.Spec Number c
-button = H.append (H.mkSpec render foldState)
+ui :: Signal1 ButtonEvent (HTML ButtonEvent)
+ui = render <$> stateful 0 (\n _ -> n + 1)
   where
-  render :: H.Render Number (Maybe c) ButtonEvent
-  render _ = H.button [H.OnClick (const Click)] [H.child Nothing]
-  
-  foldState :: H.FoldState Number ButtonEvent
-  foldState count Click = count + 1
-  
--- | This `Spec` defines a reusable label component, which supports neither child
--- | nodes nor events.
--- |
--- | Its state is anything which can be rendered as a string.
-label :: forall a. (Show a) => H.Spec a H.Void
-label = H.mkSpec render foldState
-  where
-  render :: H.Render a H.Void H.Void
-  render a = H.text (show a)
-  
-  foldState :: H.FoldState a H.Void
-  foldState st _ = st
-  
--- | The UI is composed by inserting a label as the button's first (only) child, and then 
--- | placing the button next to _another_ label in a `div` element.
--- |
--- | We use the `label` component polymorphically. In the first case, it is used to render 
--- | the click count of type number. In the second case, it is used to render a string.
--- |
--- | The state type becomes a `Tuple` due to the use of the `beside` combinator. 
-ui :: H.Component (Tuple Number String)
-ui = button label `H.beside` label
+  render :: Number -> HTML ButtonEvent
+  render n = button [OnClick (const Click)] [text (show n)]
 
 main = do
-  node <- H.runComponent ui (Tuple 0 "Click the button")
+  node <- runUI ui
   appendToBody node
