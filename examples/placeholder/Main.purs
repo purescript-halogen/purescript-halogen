5df0050448d5bb77db7708a75a2f3ff2182ab148
Phil Freeman <paf31@cantab.net>
2015-03-18 21:26:50 -0700

    Improve placeholder example, fix #41

--- examples/placeholder/Main.purs
+++ examples/placeholder/Main.purs
@@ -1,6 +1,7 @@
 module Example.Placeholder where
 
 import Data.Void
+import Data.Maybe
 import Data.Tuple
 
 import Control.Monad.Eff
@@ -9,7 +10,7 @@ import DOM
 
 import Halogen
 import Halogen.Signal
-import Halogen.Internal.VirtualDOM (VTree(), vtext)
+import Halogen.Internal.VirtualDOM (VTree(), widget)
 
 import qualified Halogen.HTML as H
 import qualified Halogen.HTML.Attributes as A
@@ -23,19 +24,50 @@ foreign import appendToBody
   \  };\
   \}" :: forall eff. Node -> Eff (dom :: DOM | eff) Node
 
+-- | An example of a function which creates a DOM node to 
+-- | be embedded inside a HTML document.
+foreign import createParagraph
+  "function createParagraph(s) {\
+  \  return function() {\
+  \    var p = document.createElement('p');\
+  \    p.style.color = 'red';\
+  \    p.appendChild(document.createTextNode(s));\
+  \    return p;\
+  \  };\
+  \}" :: forall eff. String -> Eff (dom :: DOM | eff) Node
+
+-- | Instead of embedding nodes directly, we create a placeholder type,
+-- | and insert placeholders into our document.
+-- |
+-- | This way, we keep our view pure, and can separate out impure code into the
+-- | renderer function, below.
 data Placeholder = Placeholder String
 
 view :: forall r. H.HTML Placeholder r
 view = H.div (A.class_ B.container)
           [ H.h1_ [ H.text "placeholder" ]
-          , H.placeholder (Placeholder "This text rendered by renderPlaceholder.")
-          ]
+          , H.placeholder (Placeholder "A custom widget will be rendered here.")
+          ] 
+  
+-- | Our placeholder will be replaced with a widget, which we will create using the `widget`
+-- | function from `Halogen.Internal.VirtualDOM`.
+makeWidget :: String -> VTree  
+makeWidget s = widget init update destroy
+  where 
+  init :: forall eff. Eff (dom :: DOM | eff) Node
+  init = createParagraph s
+      
+  update :: forall eff. Node -> Eff (dom :: DOM | eff) (Maybe Node)
+  update _ = pure Nothing
+  
+  destroy :: forall eff. Node -> Eff (dom :: DOM | eff) Unit
+  destroy _ = return unit
   
 -- | Convert placeholders into `VTrees`.
 -- |
 -- | In a real application, this function might render a third party component using `widget`.
 renderer :: Placeholder -> VTree
-renderer (Placeholder s) = vtext s  
+renderer (Placeholder s) = makeWidget s  
   
 ui :: forall i eff. UI i Placeholder Void eff
 ui = { view: pure view, handler: absurd, renderer: renderer }    
