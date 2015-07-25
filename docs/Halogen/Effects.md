91f7352fbee6da9d3480e8ee4d73a73c7b0c343b
Gary Burgess <gary.burgess@gmail.com>
2015-07-25 12:43:44 +0100

    Remove unused console effect, add docs

--- docs/Halogen/Effects.md
+++ docs/Halogen/Effects.md
@@ -3,7 +3,20 @@
 #### `HalogenEffects`
 
 ``` purescript
-type HalogenEffects eff = (dom :: DOM, avar :: AVAR, console :: CONSOLE, err :: EXCEPTION | eff)
+type HalogenEffects eff = (dom :: DOM, avar :: AVAR, err :: EXCEPTION | eff)
+```
+
+A type alias for the basic row of effects that Halogen uses. This can be
+combined with app-specific effects to help keep type signatures more
+manageable:
+
+```purescript
+type AppEffects eff = HalogenEffects ( ajax :: AJAX, console :: CONSOLE | eff )
+
+main :: Eff (AppEffects ()) Unit
+main = launchAff $ do
+  app <- runUI app state
+  ...
 ```
 
 
--- src/Halogen/Effects.purs
+++ src/Halogen/Effects.purs
@@ -1,15 +1,24 @@
 module Halogen.Effects where
 
 import Control.Monad.Aff.AVar (AVAR())
-import Control.Monad.Eff.Console (CONSOLE())
 import Control.Monad.Eff.Exception (EXCEPTION())
-
 import DOM (DOM())
 
+-- | A type alias for the basic row of effects that Halogen uses. This can be
+-- | combined with app-specific effects to help keep type signatures more
+-- | manageable:
+-- |
+-- | ```purescript
+-- | type AppEffects eff = HalogenEffects ( ajax :: AJAX, console :: CONSOLE | eff )
+-- |
+-- | main :: Eff (AppEffects ()) Unit
+-- | main = launchAff $ do
+-- |   app <- runUI app state
+-- |   ...
+-- | ```
 type HalogenEffects eff =
   ( dom :: DOM
   , avar :: AVAR
-  , console :: CONSOLE
   , err :: EXCEPTION
   | eff
   )
