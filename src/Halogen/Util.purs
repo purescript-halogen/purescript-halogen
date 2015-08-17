8e94eb70094c7970bb3540f18bf3299d0d11792f
Gary Burgess <gary.burgess@gmail.com>
2015-08-17 11:24:51 +0100

    Await load event in appendToBody

--- src/Halogen/Util.purs
+++ src/Halogen/Util.purs
@@ -10,15 +10,22 @@ import Data.Maybe (Maybe(..))
 import Data.Nullable (toMaybe)
 
 import DOM (DOM())
+import DOM.Event.EventTarget (eventListener, addEventListener)
+import DOM.Event.EventTypes (load)
+import DOM.Event.Types (Event())
 import DOM.HTML (window)
-import DOM.HTML.Window (document)
 import DOM.HTML.Document (body)
-import DOM.HTML.Types (HTMLElement(), htmlElementToNode)
+import DOM.HTML.Types (HTMLElement(), htmlElementToNode, windowToEventTarget)
+import DOM.HTML.Window (document)
 import DOM.Node.Node (appendChild)
 
 appendToBody :: forall m eff. (MonadEff (dom :: DOM | eff) m) => HTMLElement -> m Unit
 appendToBody e = liftEff $ do
-  b <- toMaybe <$> ((body <=< document) =<< window)
-  case b of
-    Nothing -> pure unit
-    Just b' -> void $ appendChild (htmlElementToNode e) (htmlElementToNode b')
+  addEventListener load (eventListener onLoad) false <<< windowToEventTarget =<< window
+  where
+  onLoad :: Event -> Eff (dom :: DOM) Unit
+  onLoad _ = do
+    b <- toMaybe <$> ((body <=< document) =<< window)
+    case b of
+      Nothing -> pure unit
+      Just b' -> void $ appendChild (htmlElementToNode e) (htmlElementToNode b')
