198aa90478328763889539942f702488892e11f3
Gary Burgess <gary.burgess@gmail.com>
2016-11-22 23:44:55 +0000

    Fix eff-random example

--- examples/effects-eff-random/src/Main.purs
+++ examples/effects-eff-random/src/Main.purs
@@ -2,13 +2,12 @@ module Main where
 
 import Prelude
 import Control.Monad.Eff (Eff)
-import Halogen as H
-import Halogen.Util (runHalogenAff, awaitBody)
+import Halogen.Aff as HA
 import Halogen.VirtualDOM.Driver (runUI)
 import Control.Monad.Eff.Random (RANDOM)
 import Component (ui)
 
-main :: Eff (H.HalogenEffects (random :: RANDOM)) Unit
-main = runHalogenAff do
-  body <- awaitBody
+main :: Eff (HA.HalogenEffects (random :: RANDOM)) Unit
+main = HA.runHalogenAff do
+  body <- HA.awaitBody
   runUI ui body
