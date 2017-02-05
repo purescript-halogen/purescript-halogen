571969169b43e62cedd6936b0ebfa038376bcb1e
Gary Burgess <gary.burgess@gmail.com>
2017-02-05 18:58:29 +0000

    Add component inputs example

---
+++ examples/components-inputs/README.md
@@ -0,0 +1,22 @@
+## Components with inputs example
+
+This example illustrates a component with an input value. When the parent re-renders the child component will receive the new input value and update accordingly.
+
+### Building
+
+From the root of the Halogen project:
+
+```
+$ npm install
+$ npm run example-components-inputs
+```
+
+Or from the current directory:
+
+```
+$ npm install
+$ bower install
+$ npm run build
+```
+
+The code will be built as `example.js` in the `examples/components-inputs/dist` directory within the example, runnable by opening the corresponding `index.html`.
---
+++ examples/components-inputs/src/Container.purs
@@ -0,0 +1,59 @@
+module Container where
+
+import Prelude
+import Data.Maybe (Maybe(..))
+import Halogen as H
+import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
+import Display as Display
+
+data Query a
+  = Increment a
+  | Decrement a
+
+type State = Int
+
+newtype Slot = Slot Int
+
+derive newtype instance eqSlot :: Eq Slot
+derive newtype instance ordSlot :: Ord Slot
+
+component :: forall m. H.Component HH.HTML Query Unit Void m
+component =
+  H.parentComponent
+    { initialState: const initialState
+    , render
+    , eval
+    , receiver: const Nothing
+    }
+  where
+
+  initialState :: State
+  initialState = 0
+
+  render :: State -> H.ParentHTML Query Display.Query Slot m
+  render state =
+    HH.div_
+      [ HH.ul_
+          [ HH.slot (Slot 1) Display.component state absurd
+          , HH.slot (Slot 2) Display.component (state * 2) absurd
+          , HH.slot (Slot 3) Display.component (state * 3) absurd
+          , HH.slot (Slot 4) Display.component (state * 10) absurd
+          , HH.slot (Slot 5) Display.component (state * state) absurd
+          ]
+      , HH.button
+          [ HE.onClick (HE.input_ Increment) ]
+          [ HH.text "+1"]
+      , HH.button
+          [ HE.onClick (HE.input_ Decrement) ]
+          [ HH.text "-1"]
+      ]
+
+  eval :: Query ~> H.ParentDSL State Query Display.Query Slot Void m
+  eval = case _ of
+    Increment next -> do
+      H.modify (_ + 1)
+      pure next
+    Decrement next -> do
+      H.modify (_ - 1)
+      pure next
