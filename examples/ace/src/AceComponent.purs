aacd2755905ff2a86b96093f96dcf6b04f33c1e1
Gary Burgess <gary.burgess@gmail.com>
2017-02-23 21:36:29 +0000

    Fix Ace example to not use a parent component

--- examples/ace/src/AceComponent.purs
+++ examples/ace/src/AceComponent.purs
@@ -6,7 +6,6 @@ import Control.Monad.Aff (Aff)
 import Control.Monad.Aff.AVar (AVAR)
 
 import Data.Maybe (Maybe(..))
-import Data.Const (Const)
 
 import Halogen as H
 import Halogen.HTML as HH
@@ -37,7 +36,7 @@ type AceEffects eff = (ace :: ACE, avar :: AVAR | eff)
 -- | The Ace component definition.
 aceComponent :: forall eff. H.Component HH.HTML AceQuery Unit AceOutput (Aff (AceEffects eff))
 aceComponent =
-  H.lifecycleParentComponent
+  H.lifecycleComponent
     { initialState: const initialState
     , render
     , eval
@@ -48,48 +47,48 @@ aceComponent =
   where
 
   initialState :: AceState
-  initialState =
-    { editor: Nothing }
+  initialState = { editor: Nothing }
 
   -- As we're embedding a 3rd party component we only need to create a
   -- placeholder div here and attach the ref property which will let us reference
   -- the element in eval.
-  render :: AceState -> H.ParentHTML AceQuery (Const Void) Unit (Aff (AceEffects eff))
+  render :: AceState -> H.ComponentHTML AceQuery
   render = const $ HH.div [ HP.ref (H.RefLabel "ace") ] []
 
   -- The query algebra for the component handles the initialization of the Ace
   -- editor as well as responding to the `ChangeText` action that allows us to
   -- alter the editor's state.
-  eval :: AceQuery ~> H.ParentDSL AceState AceQuery (Const Void) Unit AceOutput (Aff (AceEffects eff))
-  eval (Initialize next) = do
-    H.getHTMLElementRef (H.RefLabel "ace") >>= case _ of
-      Nothing -> pure unit
-      Just el' -> do
-        editor <- H.liftEff $ Ace.editNode el' Ace.ace
-        session <- H.liftEff $ Editor.getSession editor
-        H.modify (_ { editor = Just editor })
-        H.subscribe $ H.eventSource_ (Session.onChange session) (H.request HandleChange)
-    pure next
-  eval (Finalize next) = do
-    -- Release the reference to the editor and do any other cleanup that a real
-    -- world component might need.
-    H.modify (_ { editor = Nothing })
-    pure next
-  eval (ChangeText text next) = do
-    maybeEditor <- H.gets _.editor
-    case maybeEditor of
-      Nothing -> pure unit
-      Just editor -> do
-        current <- H.liftEff $ Editor.getValue editor
-        when (text /= current) do
-          void $ H.liftEff $ Editor.setValue text Nothing editor
-    H.raise $ TextChanged text
-    pure next
-  eval (HandleChange reply) = do
-    maybeEditor <- H.gets _.editor
-    case maybeEditor of
-      Nothing -> pure unit
-      Just editor -> do
-        text <- H.liftEff (Editor.getValue editor)
-        H.raise $ TextChanged text
-    pure (reply H.Listening)
+  eval :: AceQuery ~> H.ComponentDSL AceState AceQuery AceOutput (Aff (AceEffects eff))
+  eval = case _ of
+    Initialize next -> do
+      H.getHTMLElementRef (H.RefLabel "ace") >>= case _ of
+        Nothing -> pure unit
+        Just el' -> do
+          editor <- H.liftEff $ Ace.editNode el' Ace.ace
+          session <- H.liftEff $ Editor.getSession editor
+          H.modify (_ { editor = Just editor })
+          H.subscribe $ H.eventSource_ (Session.onChange session) (H.request HandleChange)
+      pure next
+    Finalize next -> do
+      -- Release the reference to the editor and do any other cleanup that a
+      -- real world component might need.
+      H.modify (_ { editor = Nothing })
+      pure next
+    ChangeText text next -> do
+      maybeEditor <- H.gets _.editor
+      case maybeEditor of
+        Nothing -> pure unit
+        Just editor -> do
+          current <- H.liftEff $ Editor.getValue editor
+          when (text /= current) do
+            void $ H.liftEff $ Editor.setValue text Nothing editor
+      H.raise $ TextChanged text
+      pure next
+    HandleChange reply -> do
+      maybeEditor <- H.gets _.editor
+      case maybeEditor of
+        Nothing -> pure unit
+        Just editor -> do
+          text <- H.liftEff (Editor.getValue editor)
+          H.raise $ TextChanged text
+      pure (reply H.Listening)
