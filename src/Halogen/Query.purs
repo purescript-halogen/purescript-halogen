919f9f083d52fbfec7f44728e0cd6b3321d0e27e
Gary Burgess <gary.burgess@gmail.com>
2018-01-14 12:23:07 +0000

    Re-export fork function

--- src/Halogen/Query.purs
+++ src/Halogen/Query.purs
@@ -31,7 +31,7 @@ import DOM.HTML.Types (HTMLElement, readHTMLElement)
 
 import Halogen.Component.ChildPath (ChildPath, injSlot, prjSlot, injQuery, cpI)
 import Halogen.Query.EventSource (EventSource, SubscribeStatus(..), eventSource, eventSource_)
-import Halogen.Query.HalogenM (HalogenM(..), HalogenF(..), getRef, getSlots, checkSlot, mkQuery)
+import Halogen.Query.HalogenM (HalogenM(..), HalogenF(..), fork, getRef, getSlots, checkSlot, mkQuery)
 
 import Control.Parallel (parTraverse)
 import Control.Monad.Aff.Class (liftAff) as Exports
