c08958cef7f373c674543d303f80ae0d4ba4eec2
Gary Burgess <gary.burgess@gmail.com>
2018-06-10 23:32:28 +0100

    Restore Profunctor newtype for Component

---
+++ src/Halogen/Component/Profunctor.purs
@@ -0,0 +1,30 @@
+module Halogen.Component.Profunctor (ProComponent(..)) where
+
+import Prelude
+
+import Data.Bifunctor (lmap)
+import Data.Newtype (class Newtype)
+import Data.Profunctor (class Profunctor, dimap)
+import Halogen.Component (Component, ComponentSpec', component', unComponent)
+import Halogen.Query.HalogenM as HM
+
+newtype ProComponent h f m i o = ProComponent (Component h f i o m)
+
+derive instance newtypeProComponent :: Newtype (ProComponent h f m i o) _
+
+instance profunctorProComponent :: Functor f => Profunctor (ProComponent h f m) where
+  dimap f g (ProComponent c) =
+    ProComponent (unComponent (component' <<< dimapSpec f g) c)
+
+dimapSpec
+  :: forall h s f act ps i j o p m
+   . Functor f
+  => (j -> i)
+  -> (o -> p)
+  -> ComponentSpec' h s f act ps i o m
+  -> ComponentSpec' h s f act ps j p m
+dimapSpec f g spec =
+  { initialState: spec.initialState <<< f
+  , render: spec.render
+  , eval: dimap (lmap f) (HM.mapOutput g) spec.eval
+  }
