8672c458838b4fcd6d27f2e7519893cde63c2d44
Gary Burgess <gary.burgess@gmail.com>
2018-06-27 14:26:53 +0100

    Add test fork fork/kill in eval

---
+++ test/Test/Component/ForkTest.purs
@@ -0,0 +1,90 @@
+module Test.Component.ForkTest where
+
+import Prelude
+
+import Control.Coroutine as CR
+import Data.Foldable (traverse_)
+import Data.List ((:))
+import Data.List as L
+import Data.Maybe (Maybe(..))
+import Effect.Aff (Aff)
+import Effect.Aff as Aff
+import Effect.Class.Console (log)
+import Effect.Ref as Ref
+import Halogen as H
+import Test.Assert (assertEqual)
+import Test.TestDriver as TD
+
+type State = Maybe H.ForkId
+
+data Query a
+  = StartFork a
+  | KillFork a
+
+newtype Message = Message String
+
+derive newtype instance eqMessage :: Eq Message
+derive newtype instance showMessage :: Show Message
+
+component :: H.Component TD.TestRenderProduct Query Unit Message Aff
+component =
+  H.component
+    { initialState: const Nothing
+    , render: TD.render
+    , eval
+    , receiver: const Nothing
+    , initializer: Nothing
+    , finalizer: Nothing
+    }
+  where
+  eval :: Query ~> H.HalogenM State Query () Message Aff
+  eval = case _ of
+    StartFork a -> do
+      let
+        loop = do
+          H.liftAff $ Aff.delay (Aff.Milliseconds 100.0)
+          H.raise (Message "Progress")
+          loop
+      H.raise (Message "Starting")
+      fid <- H.fork loop
+      H.put (Just fid)
+      pure a
+    KillFork a -> do
+      H.get >>= traverse_ \fid -> do
+        H.put Nothing
+        H.kill fid
+      H.raise (Message "Killed")
+      pure a
+
+test :: Aff Unit
+test = do
+  io <- TD.runUI component unit
+
+  logRef <- H.liftEffect $ Ref.new L.Nil
+
+  io.subscribe $ CR.consumer \msg -> do
+    log (show msg)
+    H.liftEffect $ Ref.modify_ (msg : _) logRef
+    pure Nothing
+
+  io.query (H.action StartFork)
+  Aff.delay (Aff.Milliseconds 350.0)
+  io.query (H.action KillFork)
+
+  -- TODO: revisit this: why do we need to wait to receive `raise`d messages
+  -- from the component, if the `raise` occurs after any bind?
+  Aff.delay (Aff.Milliseconds 15.0)
+
+  logOut <- L.reverse <$> H.liftEffect (Ref.read logRef)
+
+  H.liftEffect $ assertEqual
+    { expected:
+        Message <$>
+          "Starting"
+            : "Progress"
+            : "Progress"
+            : "Progress"
+            : "Killed"
+            : L.Nil
+    , actual: logOut
+    }
