65cb9669aca4ca111d1df2e660a2f339e2b324ec
Gary Burgess <gary.burgess@gmail.com>
2018-12-02 22:00:55 +0000

    Update effects examples

--- examples/effects-eff-random/src/Component.purs
+++ examples/effects-eff-random/src/Component.purs
@@ -1,48 +1,44 @@
 module Example.Effects.Eff.Random.Component where
 
 import Prelude
-import Effect.Aff (Aff)
-import Effect.Random (random)
+
 import Data.Maybe (Maybe(..), maybe)
+import Effect.Class (class MonadEffect)
+import Effect.Random (random)
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 
+data Action = Regenerate
+
 type State = Maybe Number
 
-data Query a = Regenerate a
-
-ui :: H.Component HH.HTML Query Unit Void Aff
-ui =
-  H.component
-    { initialState: const initialState
+component :: forall f i o m. MonadEffect m => H.Component HH.HTML f i o m
+component =
+  H.mkComponent
+    { initialState
     , render
-    , eval
-    , receiver: const Nothing
-    , initializer: Nothing
-    , finalizer: Nothing
+    , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
     }
-  where
 
-  initialState :: State
-  initialState = Nothing
+initialState :: forall i. i -> State
+initialState _ = Nothing
 
-  render :: forall m. State -> H.ComponentHTML (Query Unit) () m
-  render state =
-    let
-      value = maybe "No number generated yet" show state
-    in
-      HH.div_ $
-        [ HH.h1_ [ HH.text "Random number" ]
-        , HH.p_ [ HH.text ("Current value: " <> value) ]
-        , HH.button
-            [ HE.onClick (HE.input_ Regenerate) ]
-            [ HH.text "Generate new number" ]
-        ]
+render :: forall m. State -> H.ComponentHTML Action () m
+render state =
+  let
+    value = maybe "No number generated yet" show state
+  in
+    HH.div_ $
+      [ HH.h1_ [ HH.text "Random number" ]
+      , HH.p_ [ HH.text ("Current value: " <> value) ]
+      , HH.button
+          [ HE.onClick \_ -> Just Regenerate ]
+          [ HH.text "Generate new number" ]
+      ]
 
-  eval :: Query ~> H.HalogenM State (Query Unit) () Void Aff
-  eval = case _ of
-    Regenerate next -> do
-      newNumber <- H.liftEffect random
-      H.put (Just newNumber)
-      pure next
+handleAction :: forall o m. MonadEffect m => Action -> H.HalogenM State Action () o m Unit
+handleAction = case _ of
+  Regenerate -> do
+    newNumber <- H.liftEffect random
+    H.put (Just newNumber)
--- examples/effects-eff-random/src/Main.purs
+++ examples/effects-eff-random/src/Main.purs
@@ -3,11 +3,11 @@ module Example.Effects.Eff.Random.Main where
 import Prelude
 
 import Effect (Effect)
-import Example.Effects.Eff.Random.Component (ui)
+import Example.Effects.Eff.Random.Component (component)
 import Halogen.Aff as HA
 import Halogen.VDom.Driver (runUI)
 
 main :: Effect Unit
 main = HA.runHalogenAff do
   body <- HA.awaitBody
-  runUI ui unit body
+  runUI component unit body
