b031c69e3e5205a88b2323beda72ec0c55386818
Christoph <christoph.hegemann1337@gmail.com>
2019-02-16 14:40:20 +0100

    Implements a ReaderT example for "interpret"

--- examples/interpret/src/Main.purs
+++ examples/interpret/src/Main.purs
@@ -2,66 +2,82 @@ module Example.Interpret.Main where
 
 import Prelude
 
-import Control.Monad.Free (Free, liftF, foldFree)
-import Data.Maybe (Maybe(..))
+import Control.Monad.Reader (ReaderT, ask, runReaderT)
+import Control.Monad.Trans.Class (lift)
+import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Newtype (class Newtype)
 import Effect (Effect)
 import Effect.Aff (Aff)
-import Effect.Console (log)
-import Halogen (liftEffect)
 import Halogen as H
 import Halogen.Aff as HA
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Halogen.VDom.Driver (runUI)
+import Network.HTTP.Affjax as AX
+import Network.HTTP.Affjax.Response as AXResponse
 
-type State = { on :: Boolean }
+type Env = { githubToken :: Maybe String }
 
-data Query a = ToggleState a
+newtype App a = App (ReaderT Env Aff a)
+derive instance newtypeApp ∷ Newtype (App a) _
+derive newtype instance functorApp ∷ Functor App
+derive newtype instance applyApp ∷ Apply App
+derive newtype instance applicativeApp ∷ Applicative App
+derive newtype instance bindApp ∷ Bind App
+derive newtype instance monadApp ∷ Monad App
 
-data MyAlgebra a = Log String a
-type MyMonad = Free MyAlgebra
+runApp :: forall a. Env -> App a -> Aff a
+runApp env (App app) = runReaderT app env
 
-output :: String -> MyMonad Unit
-output msg = liftF (Log msg unit)
+class Monad m <= GithubAPI m where
+  searchUser ∷ String → m String
 
-ui :: H.Component HH.HTML Query Unit Void MyMonad
+instance githubAPI ∷ GithubAPI App where
+  searchUser q = App do
+    { githubToken } <- ask
+    { response } <- case githubToken of
+      Nothing ->
+        lift $ AX.get AXResponse.string ("https://api.github.com/users/" <> q)
+      Just token ->
+        lift $ AX.get AXResponse.string ("https://api.github.com/users/" <> q <> "?access_token=" <> token)
+    pure response
+
+type State = { userData :: Maybe String }
+
+data Action = FetchData
+
+ui :: forall f i o m. GithubAPI m => H.Component HH.HTML f i o m
 ui =
-  H.component
+  H.mkComponent
     { initialState: const initialState
     , render
-    , eval
-    , receiver: const Nothing
-    , initializer: Nothing
-    , finalizer: Nothing
+    , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
     }
   where
 
   initialState :: State
-  initialState = { on: false }
+  initialState = { userData: Nothing }
 
-  render :: forall m. State -> H.ComponentHTML (Query Unit) () m
+  render :: State -> H.ComponentHTML Action () m
   render state =
     HH.div_
       [ HH.h1_
-          [ HH.text "Toggle Button" ]
+          [ HH.text "Fetch user data" ]
       , HH.button
-          [ HE.onClick (HE.input_ ToggleState) ]
-          [ HH.text (if state.on then "On" else "Off") ]
+          [ HE.onClick \_ -> Just FetchData ]
+          [ HH.text "Fetch" ]
+      , HH.p_
+          [ HH.text (fromMaybe "No user data" state.userData) ]
       ]
 
-  eval :: Query ~> H.HalogenM State (Query Unit) () Void MyMonad
-  eval (ToggleState next) = do
-    H.modify_ (\state -> { on: not state.on })
-    H.lift $ output "State was toggled"
-    pure next
+handleAction :: forall o m. GithubAPI m => Action -> H.HalogenM State Action () o m Unit
+handleAction = case _ of
+  FetchData -> do
+    userData <- lift $ searchUser "kRITZCREEK"
+    H.put { userData: Just userData }
 
-ui' :: H.Component HH.HTML Query Unit Void Aff
-ui' = H.hoist (foldFree evalMyAlgebra) ui
-  where
-  evalMyAlgebra :: MyAlgebra ~> Aff
-  evalMyAlgebra (Log msg next) = do
-    liftEffect $ log msg
-    pure next
+ui' :: forall f i o. H.Component HH.HTML f i o Aff
+ui' = H.hoist (runApp { githubToken: Nothing }) ui
 
 main :: Effect Unit
 main = HA.runHalogenAff do
