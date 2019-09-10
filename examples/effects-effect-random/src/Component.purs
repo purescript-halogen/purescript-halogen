80a7b0df8b38b0d12240309fb876971cfba7ceed
Gary Burgess <gary.burgess@gmail.com>
2019-09-10 22:55:07 +0100

    `f` -> `q` type variable in examples

--- examples/effects-effect-random/src/Component.purs
+++ examples/effects-effect-random/src/Component.purs
@@ -13,7 +13,7 @@ data Action = Regenerate
 
 type State = Maybe Number
 
-component :: forall f i o m. MonadEffect m => H.Component f i o m
+component :: forall q i o m. MonadEffect m => H.Component q i o m
 component =
   H.mkComponent
     { initialState
