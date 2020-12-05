bffbea4617e649aab6ccf178efb19a04f6d71a89
matoruru <matoruru@shgt.jp>
2020-12-05 11:31:08 +0900

    Add `id_` back as the deprecated version of `id`

--- src/Halogen/HTML/Properties.purs
+++ src/Halogen/HTML/Properties.purs
@@ -21,6 +21,7 @@ module Halogen.HTML.Properties
   , width
   , href
   , id
+  , id_
   , name
   , rel
   , src
@@ -88,6 +89,7 @@ import Halogen.HTML.Core (class IsProp, AttrName(..), ClassName, Namespace, Prop
 import Halogen.HTML.Core as Core
 import Halogen.Query.Input (Input(..), RefLabel)
 import Prim.Row as Row
+import Prim.TypeError as TypeError
 import Unsafe.Coerce (unsafeCoerce)
 import Web.DOM.Element (Element)
 
@@ -179,6 +181,9 @@ href = prop (PropName "href")
 
 id :: forall r i. String -> IProp (id :: String | r) i
 id = prop (PropName "id")
+
+id_ :: forall r i. TypeError.Warn (TypeError.Text "`id_` is deprecated. Prefer `id` instead.") => String -> IProp (id :: String | r) i
+id_ = id
 
 name :: forall r i. String -> IProp (name :: String | r) i
 name = prop (PropName "name")
