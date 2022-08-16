7610b4cef2f2d3c99d6058bc57fb34b81ee6e740
Rowan Skewes <rowan.skewes@gmail.com>
2022-08-16 20:40:30 +1000

    New example for websockets: Keep demo of DriverIO

--- examples/driver-websockets/src/Log.purs
+++ examples/driver-websockets/src/Log.purs
@@ -2,52 +2,44 @@ module Example.Driver.Websockets.Log where
 
 import Prelude
 
-import Control.Monad.Except (runExcept)
-import Data.Either (hush)
 import Data.Array as A
 import Data.Maybe (Maybe(..))
-import Effect.Aff.Class (class MonadAff)
-import Foreign (readString)
+import Effect.Class (class MonadEffect)
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
-import Halogen.Query.Event (eventListener)
-import Web.Event.Event as E
-import Web.Socket.Event.EventTypes as WSET
-import Web.Socket.Event.MessageEvent as ME
-import Web.Socket.WebSocket as WS
-import Web.Socket.ReadyState as WSS
+import Web.Event.Event (Event)
+import Web.Event.Event as Event
 
-ws_url :: String
-ws_url = "wss://ws.ifelse.io"
+type Slot = H.Slot Query Message
+
+data Query a = ReceiveMessage String a
+
+data Message = OutputMessage String
+
+data Action
+  = HandleInput String
+  | Submit Event
 
 type State =
   { messages :: Array String
   , inputText :: String
-  , websocket :: Maybe WS.WebSocket
   }
 
-data Action
-  = Connect
-  | LogConnectionState String
-  | HandleInput String
-  | ReceiveMessage String
-  | Submit E.Event
-
-component :: forall query input output m. MonadAff m => H.Component query input output m
+component :: forall i m. MonadEffect m => H.Component Query i Message m
 component =
   H.mkComponent
     { initialState
     , render
     , eval: H.mkEval $ H.defaultEval
         { handleAction = handleAction
-        , initialize = Just Connect
+        , handleQuery = handleQuery
         }
     }
 
 initialState :: forall i. i -> State
-initialState _ = { messages: [], inputText: "", websocket: Nothing }
+initialState _ = { messages: [], inputText: "" }
 
 render :: forall m. State -> H.ComponentHTML Action () m
 render state =
@@ -64,48 +56,23 @@ render state =
         [ HH.text "Send Message" ]
     ]
 
-handleAction :: forall output m. MonadAff m => Action -> H.HalogenM State Action () output m Unit
+handleAction :: forall m. MonadEffect m => Action -> H.HalogenM State Action () Message m Unit
 handleAction = case _ of
-  Connect -> do
-    connection <- H.liftEffect $ WS.create ws_url []
-    H.modify_ (_ { websocket = Just connection })
-    addWsListener connection WSET.onOpen \_ -> Just $ LogConnectionState "Open"
-    addWsListener connection WSET.onClose \_ -> Just $ LogConnectionState "Closed"
-    addWsListener connection WSET.onError \_ -> Just $ LogConnectionState "Error"
-    addWsListener connection WSET.onMessage (map ReceiveMessage)
-    where
-    addWsListener :: WS.WebSocket -> E.EventType -> (Maybe String -> Maybe Action) -> H.HalogenM State Action () output m Unit
-    addWsListener connection eventType messageHandler =
-      void $ H.subscribe $ eventListener
-        eventType
-        (WS.toEventTarget connection)
-        (wsMessageFromEvent >>> messageHandler)
-
-    wsMessageFromEvent :: E.Event -> Maybe String
-    wsMessageFromEvent event =
-      ME.fromEvent event >>= (ME.data_ >>> readString >>> runExcept >>> hush)
-  LogConnectionState connectionState -> do
-    let logMessage = "Connection state: " <> connectionState
-    H.modify_ \st -> st { messages = st.messages `A.snoc` logMessage }
   HandleInput text -> do
     H.modify_ (_ { inputText = text })
-  ReceiveMessage msg -> do
-    let incomingMessage = "Received: " <> msg
-    H.modify_ \st -> st { messages = st.messages `A.snoc` incomingMessage }
   Submit ev -> do
-    H.liftEffect $ E.preventDefault ev
+    H.liftEffect $ Event.preventDefault ev
     st <- H.get
     let outgoingMessage = st.inputText
-    logMessage <- H.liftEffect $ case st.websocket of
-      Just connection -> do
-        connectionState <- WS.readyState connection
-        case connectionState of
-          WSS.Open -> do
-            WS.sendString connection outgoingMessage
-            pure $ "Sending: " <> outgoingMessage
-          _ -> pure "Unable to send: connection not open"
-      Nothing -> pure "Unable to send: connection not initialized"
+    H.raise $ OutputMessage outgoingMessage
     H.modify_ \st' -> st'
-      { messages = st'.messages `A.snoc` logMessage
+      { messages = st'.messages `A.snoc` ("Sending: " <> outgoingMessage)
       , inputText = ""
       }
+
+handleQuery :: forall m a. Query a -> H.HalogenM State Action () Message m (Maybe a)
+handleQuery = case _ of
+  ReceiveMessage msg a -> do
+    let incomingMessage = "Received: " <> msg
+    H.modify_ \st -> st { messages = st.messages `A.snoc` incomingMessage }
+    pure (Just a)
--- examples/driver-websockets/src/Main.purs
+++ examples/driver-websockets/src/Main.purs
@@ -2,12 +2,46 @@ module Example.Driver.Websockets.Main where
 
 import Prelude
 
+import Control.Monad.Except (runExcept)
+import Data.Either (hush)
+import Data.Maybe (Maybe)
 import Effect (Effect)
+import Effect.Aff (launchAff_)
 import Example.Driver.Websockets.Log as Log
+import Foreign (readString)
+import Halogen as H
 import Halogen.Aff as HA
+import Halogen.Query.Event as HQE
+import Halogen.Subscription as HS
 import Halogen.VDom.Driver (runUI)
+import Web.Event.Event as E
+import Web.Socket.Event.EventTypes as WSET
+import Web.Socket.Event.MessageEvent as WSME
+import Web.Socket.WebSocket as WS
 
 main :: Effect Unit
-main = HA.runHalogenAff do
-  body <- HA.awaitBody
-  runUI Log.component unit body
+main = do
+  connection <- WS.create "wss://ws.ifelse.io" []
+  HA.runHalogenAff do
+    body <- HA.awaitBody
+    io <- runUI Log.component unit body
+
+    -- Subscribe to all output messages from our component,
+    -- forwarding them through the websocket
+    let forwardToWebsocket = case _ of
+          Log.OutputMessage message -> WS.sendString connection message
+    _ <- H.liftEffect $ HS.subscribe io.messages forwardToWebsocket
+
+    -- Subscribe to messages from the websocket,
+    -- forwarding them to the child component
+    let websocketEmitter =
+          HQE.eventListener WSET.onMessage (WS.toEventTarget connection) wsMessageFromEvent
+        forwardToChild =
+          launchAff_ <<< void <<< io.query <<< H.mkTell <<< Log.ReceiveMessage
+    void $ H.liftEffect $ HS.subscribe websocketEmitter forwardToChild
+
+wsMessageFromEvent :: E.Event -> Maybe String
+wsMessageFromEvent event = do
+  wsEvent <- WSME.fromEvent event
+  let foreignPayload = WSME.data_ wsEvent
+  foreignPayload # (readString >>> runExcept >>> hush)
