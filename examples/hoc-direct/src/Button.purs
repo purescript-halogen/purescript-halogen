0ac8d47b0831351079f9e836015e7ba50475a238
Rory Campbell <roryc89@googlemail.com>
2023-10-10 14:51:03 +0200

    stores parent node in render #802

--- examples/hoc-direct/src/Button.purs
+++ examples/hoc-direct/src/Button.purs
@@ -7,7 +7,7 @@ import Data.Either (Either(..))
 import Data.Foldable (for_)
 import Data.Maybe (Maybe(..))
 import Data.Newtype (wrap)
-import Debug (spy)
+import Data.Tuple (Tuple(..))
 import Effect.Aff (Aff, delay)
 import Effect.Class.Console (logShow)
 import Halogen (liftAff)
@@ -16,15 +16,26 @@ import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Type.Proxy (Proxy(..))
 
-
 component :: forall q i o. H.Component q i o Aff
 component =
   H.mkComponent
-    { initialState: \_ -> { count: 0 }
-    , render: \_ -> HH.slot_ (Proxy :: _ "hoc") unit (hoc parent) unit
+    { initialState: \_ -> Tuple true true
+    , render: \(Tuple b1 b2) -> HH.div_
+        [ if b1 then HH.slot_ (Proxy :: _ "hoc") unit (hoc parent) unit else  HH.div_ []
+        , if b2 then HH.slot_ (Proxy :: _ "hocBroken") unit (hocBroken parent) unit else  HH.div_ []
+        ]
     , eval: H.mkEval H.defaultEval
+        { handleAction = handleAction
+        , initialize = Just unit
+        }
     }
 
+  where
+  handleAction _ = do
+    liftAff $ delay $ wrap 2000.0
+    H.put $ Tuple true false
+    liftAff $ delay $ wrap 2000.0
+    H.put $ Tuple false false
 
 hoc :: forall i o q. H.Component q Unit o Aff -> H.Component q i o Aff
 hoc comp =
@@ -39,16 +50,40 @@ hoc comp =
   where
   initialState _ = false
 
-  render = spy "hoc st" >>> case _ of
+  render st = HH.div_
+    [ case st of
+        false -> HH.text "Loading..."
+        _ -> HH.slot_ (Proxy :: _ "parent") unit comp unit
+    ]
+
+  handleAction _ = do
+    liftAff $ delay $ wrap 300.0
+    H.put true
+    liftAff $ delay $ wrap 300.0
+    H.put false
+
+hocBroken :: forall i o q. H.Component q Unit o Aff -> H.Component q i o Aff
+hocBroken comp =
+  H.mkComponent
+    { initialState
+    , render
+    , eval: H.mkEval H.defaultEval
+        { handleAction = handleAction
+        , initialize = Just unit
+        }
+    }
+  where
+  initialState _ = false
+
+  render = case _ of
     false -> HH.text "Loading..."
     _ -> HH.slot_ (Proxy :: _ "parent") unit comp unit
 
-  handleAction _ = do 
-    liftAff $ delay $ wrap 1000.0
+  handleAction _ = do
+    liftAff $ delay $ wrap 300.0
     H.put true
-    liftAff $ delay $ wrap 2000.0
+    liftAff $ delay $ wrap 300.0
     H.put false
-
 
 parent :: forall i o q. H.Component q i o Aff
 parent =
@@ -65,16 +100,15 @@ parent =
 
   render = case _ of
     1 -> HH.slot_ (Proxy :: _ "child1") unit child1 unit
-    2 -> HH.div [] [ HH.slot_ (Proxy :: _ "child2") unit child2 unit, HH.text "other text with 2" ]
-    3 -> HH.slot_ (Proxy :: _ "child3") unit child3 unit
+    -- 2 -> HH.div [] [ HH.slot_ (Proxy :: _ "child2") unit child2 unit, HH.text "other text with 2" ]
+    -- 3 -> HH.slot_ (Proxy :: _ "child3") unit child3 unit
     _ -> HH.div [ HE.onClick \_ -> Left unit ] [ HH.text "No child" ]
 
   handleAction = case _ of
     Right i -> H.put i
     Left _ -> void $ H.fork do
       for_ (range 1 4) \i -> do
-        logShow { i }
-        liftAff $ delay $ wrap 500.0
+        liftAff $ delay $ wrap 100.0
         H.put i
 
 child1 :: forall i o m q. H.Component q i o m
@@ -88,28 +122,4 @@ child1 =
   initialState _ = unit
 
   render _ = HH.div_ [ HH.text "Child 1" ]
-
-child2 :: forall i o m q. H.Component q i o m
-child2 =
-  H.mkComponent
-    { initialState
-    , render
-    , eval: H.mkEval H.defaultEval
-    }
-  where
-  initialState _ = unit
-
-  render _ = HH.div_ [ HH.text "Child 2" ]
-
-child3 :: forall i o m q. H.Component q i o m
-child3 =
-  H.mkComponent
-    { initialState
-    , render
-    , eval: H.mkEval H.defaultEval
-    }
-  where
-  initialState _ = unit
-
-  render _ = HH.div_ [ HH.text "Child 3" ]
 
